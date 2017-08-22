4ffbf57221ea8210ada98f4ff48c11f04e5977af
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-21 23:25:47 -0400

    Create user if no email is found, catch error cases

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,14 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -12,6 +18,9 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload }
+
     default:
       return state;
   }
