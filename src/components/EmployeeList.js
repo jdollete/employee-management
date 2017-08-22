b45779543627f8e76654b8702010c0a683dbb86e
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-22 10:28:22 -0400

    User can navigate in when logged in

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+class EmployeeList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
