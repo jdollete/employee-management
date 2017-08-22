3da4b45c4cf7fd689052be327c7c6f2b65f96335
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-22 11:07:42 -0400

    Ability to navigate to employee create form

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+class EmployeeCreate extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Employee Form</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeCreate;
