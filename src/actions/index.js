cc637537d9f9fcff330f8996a975f9ada8f31792
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-21 23:00:40 -0400

    Wire up redux-thunk to login action

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,10 +16,10 @@ export const passwordChanged = (text) => {
 };
 
 export const loginUser = ({ email, password }) => {
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  // redux-thunk will do asynchronous dispatch
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
     .then(user => console.log(user));
-  return {
-    type: ,
-    payload: text
-  };
+  }
+
 };
