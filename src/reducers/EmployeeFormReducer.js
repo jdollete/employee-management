62f06faf76d65927da016a581caba2901c512b13
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-22 11:32:41 -0400

    Refactor actions

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -3,11 +3,16 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = {
-
+  name: '',
+  phone: '',
+  shift: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name', value: 'jane'}
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
       return state;
   }
